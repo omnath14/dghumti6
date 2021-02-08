1db4a742c09a6d3360153591fa50e0886b52fe0d
Sushan <sushanyadav98@gmail.com>
2021-02-08 20:34:33 +0545

    feb-8/fixes changed the default color removed strike throught compare at price removed the carousel in homepage for now

--- client/src/core/Card.js
+++ client/src/core/Card.js
@@ -1,45 +1,53 @@
-import React, { useState } from 'react';
-import { Link, Redirect } from 'react-router-dom';
-import ShowImage from './ShowImage';
+import React, { useState } from "react";
+import { Link, Redirect } from "react-router-dom";
+import ShowImage from "./ShowImage";
 
-import { addItem, updateItem, removeItem } from './cartHelpers';
+import { addItem, updateItem, removeItem } from "./cartHelpers";
 
 const Card = ({
   product,
   showAddToCartButton = true,
   cartUpdate = false,
   showRemoveProductButton = false,
-  setRun = f => f,
-  run = undefined
-
+  setRun = (f) => f,
+  run = undefined,
 }) => {
   const [redirect, setRedirect] = useState(false);
   const [count, setCount] = useState(product.count);
 
   const addToCart = () => {
-      addItem(product, setRedirect(true));
+    addItem(product, setRedirect(true));
   };
 
-  const shouldRedirect = redirect => {
+  const shouldRedirect = (redirect) => {
     if (redirect) {
       return <Redirect to="/cart" />;
     }
   };
 
-  const showAddToCartBtn = showAddToCartButton => {
+  const showAddToCartBtn = (showAddToCartButton) => {
     return (
       showAddToCartButton && (
-          <div style={{textAlign:'center'}}>
-          <button onClick={addToCart}  className="btn mb-4"
-          style={{borderRadius:'5px',padding:'5px',width:'120px', color:'white',backgroundColor:'#008ECC' }}>Add to cart</button>
-          </div>
+        <div style={{ textAlign: "center" }}>
+          <button
+            onClick={addToCart}
+            className="btn mb-4"
+            style={{
+              borderRadius: "5px",
+              padding: "5px",
+              width: "120px",
+              color: "white",
+              backgroundColor: "#523906",
+            }}
+          >
+            Add to cart
+          </button>
+        </div>
       )
     );
   };
 
-
-
-  const handleChange = productId => event => {
+  const handleChange = (productId) => (event) => {
     setRun(!run);
     setCount(event.target.value < 0 ? 0 : event.target.value);
     if (event.target.value >= 1) {
@@ -47,7 +55,7 @@ const Card = ({
     }
   };
 
-  const showCartUpdateOptions = cartUpdate => {
+  const showCartUpdateOptions = (cartUpdate) => {
     return (
       cartUpdate && (
         <div>
@@ -55,45 +63,57 @@ const Card = ({
             <div className="input-group-prepend">
               <span className="input-group-text">Quantity</span>
             </div>
-            <input type="number" className="form-control" value={count} onChange={handleChange(product._id)} />
+            <input
+              type="number"
+              className="form-control"
+              value={count}
+              onChange={handleChange(product._id)}
+            />
           </div>
         </div>
       )
     );
   };
-  const showRemoveButton = showRemoveProductButton => {
+  const showRemoveButton = (showRemoveProductButton) => {
     return (
       showRemoveProductButton && (
-        <button onClick={() => {removeItem(product._id);setRun(!run);}}
-                className="btn btn-danger"><small>Remove from cart</small>
+        <button
+          onClick={() => {
+            removeItem(product._id);
+            setRun(!run);
+          }}
+          className="btn btn-danger"
+        >
+          <small>Remove from cart</small>
         </button>
       )
     );
   };
 
-
   return (
- <div  className="card mb-4 card-fix"  style={{border:'1px solid #008ECC',borderRadius:'13px'}} >
-{shouldRedirect(redirect)}
-  <Link to={`/product/${product._id}`}>
-<ShowImage item={product} url="product" />
-</Link>
-   <span style={{ margin:'15px'}}>Rs. <span style={{fontSize:'20px'}}>{product.price}</span> <del style={{fontSize:'14px',color:"#999999"}}>{product.cprice}</del>
-     {"    "}
-
-
-            </span>
-  <div style={{height: '45px',lineHeight: '16px', textAlign:'center'}}>
-  <small className="mb-3">{product.description.substring(0, 20)}</small>
-  </div>
-  {showCartUpdateOptions(cartUpdate)}
+    <div
+      className="card mb-4 card-fix"
+      style={{ border: "1px solid #523906", borderRadius: "13px" }}
+    >
+      {shouldRedirect(redirect)}
+      <Link to={`/product/${product._id}`}>
+        <ShowImage item={product} url="product" />
+      </Link>
+      <span style={{ margin: "15px" }}>
+        Rs. <span style={{ fontSize: "20px" }}>{product.price}</span>{" "}
+        {/* <del style={{ fontSize: "14px", color: "#999999" }}>
+          {product.cprice}
+        </del> */}
+        {"    "}
+      </span>
+      <div style={{ height: "45px", lineHeight: "16px", textAlign: "center" }}>
+        <small className="mb-3">{product.description.substring(0, 20)}</small>
+      </div>
+      {showCartUpdateOptions(cartUpdate)}
       {showRemoveButton(showRemoveProductButton)}
       {showAddToCartBtn(showAddToCartButton)}
-</div>
-
-
+    </div>
   );
 };
 
 export default Card;
-
--- client/src/core/Cart.js
+++ client/src/core/Cart.js
@@ -1,75 +1,91 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import Layout from './Layout';
-import { getCart } from './cartHelpers';
-import Card from './Card';
-import Checkout from './Checkout';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import Layout from "./Layout";
+import { getCart } from "./cartHelpers";
+import Card from "./Card";
+import Checkout from "./Checkout";
 
 const Cart = () => {
-    const [items, setItems] = useState([]);
-    const [run, setRun] = useState(false);
+  const [items, setItems] = useState([]);
+  const [run, setRun] = useState(false);
 
-    
-    useEffect(() => {
-        setItems(getCart());
-    }, [run]);
+  useEffect(() => {
+    setItems(getCart());
+  }, [run]);
 
-    const showItems = items => {
-        return (
-            <div>
-                <h6 className="card-header mb-2" style={{border:'1px solid #008ECC',borderRadius:'10px'}}>Your cart has {items.length} items</h6>
-                    <div className="alert alert-info" style={{border:'1px solid #008ECC',borderRadius:'10px'}}>
-                    <h6 className="text-dark" >Minimum Order Amount Rs 49</h6>
-                </div>
-                <div className="row row-cols-2" >
-                {items.map((product, i) => (
-                    <div key={i} className="col-sm-4">
-                    <Card product={product} showAddToCartButton={false} cartUpdate={true} showRemoveProductButton={true}
-                        setRun={setRun}
-                        run={run}/>
-                    </div>
-                ))}
-                </div>
-            </div>
-        );
-    };
-
-    const noItemsMessage = () => (
-<div className="container">
-     
-     <hr/>
-
-
-            <h6 className="card-header mb-2" style={{border:'1px solid #008ECC',borderRadius:'10px'}}>Your cart is empty.</h6>
-<hr/>
-            <Link to="/shop">
-                <h6 className="card-header mb-2" style={{border:'1px solid #008ECC',borderRadius:'10px'}}>Continue shopping</h6>
-
-            </Link>
-            <hr/>
-
-          
-</div>
-
-    );
-
+  const showItems = (items) => {
     return (
-        <Layout>
-            <div className="container">
-            <div className="row">
-                <div className="col">
-                    {items.length > 0 ? showItems(items) : noItemsMessage()}
-                </div>
-
-                <div className="col-sm-6">
-                    <div className="container">
-                        <Checkout  products={items} setRun={setRun} run={run} />
-                    </div>
-                </div>
+      <div>
+        <h6
+          className="card-header mb-2"
+          style={{ border: "1px solid #523906", borderRadius: "10px" }}
+        >
+          Your cart has {items.length} items
+        </h6>
+        <div
+          className="alert alert-info"
+          style={{ border: "1px solid #523906", borderRadius: "10px" }}
+        >
+          <h6 className="text-dark">Minimum Order Amount Rs 49</h6>
+        </div>
+        <div className="row row-cols-2">
+          {items.map((product, i) => (
+            <div key={i} className="col-sm-4">
+              <Card
+                product={product}
+                showAddToCartButton={false}
+                cartUpdate={true}
+                showRemoveProductButton={true}
+                setRun={setRun}
+                run={run}
+              />
             </div>
-            </div>
-        </Layout>
+          ))}
+        </div>
+      </div>
     );
+  };
+
+  const noItemsMessage = () => (
+    <div className="container">
+      <hr />
+
+      <h6
+        className="card-header mb-2"
+        style={{ border: "1px solid #523906", borderRadius: "10px" }}
+      >
+        Your cart is empty.
+      </h6>
+      <hr />
+      <Link to="/shop">
+        <h6
+          className="card-header mb-2"
+          style={{ border: "1px solid #523906", borderRadius: "10px" }}
+        >
+          Continue shopping
+        </h6>
+      </Link>
+      <hr />
+    </div>
+  );
+
+  return (
+    <Layout>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            {items.length > 0 ? showItems(items) : noItemsMessage()}
+          </div>
+
+          <div className="col-sm-6">
+            <div className="container">
+              <Checkout products={items} setRun={setRun} run={run} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
 };
 
 export default Cart;
