b4b400cf16f725ae6bcb3c6c159ebfccb17cb8e5
omnath14 <72333908+omnath14@users.noreply.github.com>
2021-05-18 18:41:26 +0545

    Update app.js

--- app.js
+++ app.js
@@ -9,10 +9,6 @@ const expressValidator = require('express-validator');
 require('dotenv').config();
 const path = require('path');
 
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
 connectDB();
 app.use(morgan('dev'));
 app.use(bodyParser.json());
@@ -27,23 +23,12 @@ app.use('/api', require('./routes/product'));
 app.use('/api', require('./routes/order'));
 
  
-
-
-app.use((req, res, next) => {
-    if (process.env.NODE_ENV === 'production') {
-        app.use(express.static('client/build'));
-        app.get('*',(req,res)=>{
-            res.sendFile(path.resolve(__dirname,'client','build','index.html'))
-        })
-        if (req.headers.host === 'dghumti.herokuapp.com')
-            return res.redirect(301, 'http://www.dghumti.com');
-        if (req.headers['x-forwarded-proto'] !== 'http')
-            return res.redirect('http://' + req.headers.host + req.url);
-        else
-            return next();
-    } else
-        return next();
-})
+if(process.env.NODE_ENV ==='production'){
+    app.use(express.static('client/build'));
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    })
+}
  const PORT = process.env.PORT || 7000;
 app.listen(PORT, () => {
     console.log(`A Node Js API is listening on port: ${PORT}`);
